6cf8f00430a8bcedc9bbac6c2a7578f608bd702b
waddella <adrian.waddell@roche.com>
2018-12-21 14:36:06 +0100

    add foo function

---
+++ R/foo.R
@@ -0,0 +1,21 @@
+
+
+#' Foo function
+#'
+#' a foo function
+#'
+#'
+#' @param x x argument
+#'
+#'
+#' @export
+#'
+#' @return NULL
+#'
+#' @examples
+#'
+#' foo("abcd")
+#'
+foo <- function(x) {
+  print(paste("foo:", x))
+}
