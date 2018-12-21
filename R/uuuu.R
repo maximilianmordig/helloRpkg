d62bf97aff17cfa96ada16154b635dd419c11387
waddella <adrian.waddell@roche.com>
2018-12-21 15:03:47 +0100

    fdsfads

---
+++ R/uuuu.R
@@ -0,0 +1,21 @@
+
+
+#' uuu function
+#'
+#' a uuu function
+#'
+#'
+#' @param x x argument
+#'
+#'
+#' @export
+#'
+#' @return NULL
+#'
+#' @examples
+#'
+#' uuu("abcd")
+#'
+uuu <- function(x) {
+  print(paste("uuu:", x))
+}
