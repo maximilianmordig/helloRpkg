b1004e20862b60c3383e345fc5dd8056dcb8f5a7
waddella <adrian.waddell@roche.com>
2018-12-21 14:45:51 +0100

    add bar function

---
+++ R/bar.R
@@ -0,0 +1,21 @@
+
+
+#' bar function
+#'
+#' a bar function
+#'
+#'
+#' @param x x argument
+#'
+#'
+#' @export
+#'
+#' @return NULL
+#'
+#' @examples
+#'
+#' bar("abcd", "asasdas")
+#'
+bar <- function(x) {
+  print(paste("bar:", x))
+}
