5168cc32adb940491df9e3c139922a9e0697b0d5
Maximilian Mordig <maximilian.mordig@gmail.com>
2019-01-14 11:41:53 +0100

    Experimenting with Dockerfile

---
+++ build_docker_image/setup_environment.R
@@ -0,0 +1,9 @@
+#!/usr/bin/env Rscript
+
+# used to install packages mainly
+
+args <- commandArgs()
+paste("Arg: ", args)
+
+#todo:
+#install.packages("automation.utils_0.1.0.tar.gz", repos = NULL, type="source")
