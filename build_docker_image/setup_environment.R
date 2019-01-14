ebd6fa000ce8bc00677c5385fa90e27e5dbfa332
root <root@f42dad6ff5af>
2019-01-14 13:19:47 +0000

    I am Jenkins: Making everything ready for a release with tag  from initial commit 4f5ee4894a97769e8f6f2ddba68b8f7fbd14ed1a

--- build_docker_image/setup_environment.R
+++ build_docker_image/setup_environment.R
@@ -2,4 +2,4 @@
 
 # used to install packages mainly
 
-#install.packages("automation.utils_0.1.0.tar.gz", repos = NULL, type="source")
+#install.packages("automation.utils_0.1.0.tar.gz", repos = NULL, type="source")install.packages("helloRpkg_1.0.0.tar.gz", repos = NULL, type="source")
