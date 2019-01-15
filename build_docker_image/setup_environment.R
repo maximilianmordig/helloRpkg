8c650a8c6742dcbf067c353e9805e7f921fec4dc
root <root@f42dad6ff5af>
2019-01-15 06:49:50 +0000

    I am Jenkins: Making everything ready for a release with tag v17.0.0 from initial commit ed0437068c241b9bec0c7ccdbc90fa1473495fe1

--- build_docker_image/setup_environment.R
+++ build_docker_image/setup_environment.R
@@ -1,20 +1,5 @@
 #!/usr/bin/env Rscript
 
 # used to install packages mainly
-
-#install.packages("automation.utils_0.1.0.tar.gz", repos = NULL, type="source")install.packages("helloRpkg_1.0.0.tar.gz", repos = NULL, type="source")
-install.packages("helloRpkg_2.0.0.tar.gz", repos = NULL, type="source")
-install.packages("helloRpkg_3.0.0.tar.gz", repos = NULL, type="source")
-install.packages("helloRpkg_4.0.0.tar.gz", repos = NULL, type="source")
-install.packages("helloRpkg_5.0.0.tar.gz", repos = NULL, type="source")
-install.packages("helloRpkg_6.0.0.tar.gz", repos = NULL, type="source")
-install.packages("helloRpkg_7.0.0.tar.gz", repos = NULL, type="source")
-install.packages("helloRpkg_8.0.0.tar.gz", repos = NULL, type="source")
-install.packages("helloRpkg_9.0.0.tar.gz", repos = NULL, type="source")
-install.packages("helloRpkg_10.0.0.tar.gz", repos = NULL, type="source")
-install.packages("helloRpkg_11.0.0.tar.gz", repos = NULL, type="source")
-install.packages("helloRpkg_12.0.0.tar.gz", repos = NULL, type="source")
-install.packages("helloRpkg_13.0.0.tar.gz", repos = NULL, type="source")
-install.packages("helloRpkg_14.0.0.tar.gz", repos = NULL, type="source")
-install.packages("helloRpkg_15.0.0.tar.gz", repos = NULL, type="source")
-install.packages("helloRpkg_16.0.0.tar.gz", repos = NULL, type="source")
+# use withCallingHandlers to throw an error if package not found
+withCallingHandlers(install.packages("helloRpkg_17.0.0.tar.gz", repos = NULL, type="source"), warning = function(w) stop(w))
